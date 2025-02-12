094bd5db823511bcc677fb0c9d1d4b29c5518b07
Ray Toal <rtoal@lmu.edu>
2025-02-12 13:27:39 -0800

    Progress

--- src/cassowary.js
+++ src/cassowary.js
@@ -1,5 +1,8 @@
 // Cassowary compiler
 
+// Cassowary is completely statically typed language. Also it checks
+// pretty much everything at compile time.
+
 import * as fs from "fs";
 import parse from "./parser.js";
 import translate from "./translator.js";
@@ -10,12 +13,12 @@ if (process.argv.length !== 3) {
   process.exit(1);
 }
 
-try {
-  const sourceCode = fs.readFileSync(process.argv[2], "utf8");
-  const match = parse(sourceCode);
-  const target = translate(match);
-  console.log(target.join("\n"));
-} catch (e) {
-  console.error(`${e}`);
-  process.exit(1);
-}
+// try {
+const sourceCode = fs.readFileSync(process.argv[2], "utf8");
+const match = parse(sourceCode);
+const target = translate(match);
+console.log(target.join("\n"));
+// } catch (e) {
+// console.error(`${e}`);
+process.exit(1);
+// }
--- src/translator.js
+++ src/translator.js
@@ -3,7 +3,7 @@
 export default function translate(match) {
   const grammar = match.matcher.grammar;
 
-  const locals = new Map();
+  const locals = new Map(); // string -> entity
   const target = [];
 
   function emit(line) {
@@ -16,6 +16,26 @@ export default function translate(match) {
         `${parseTreeNode.source.getLineAndColumnMessage()} ${message}`
       );
     }
+  }
+
+  function checkNumber(e, parseTreeNode) {
+    check(e.type === "number", `Expected number`, parseTreeNode);
+  }
+
+  function checkBoolean(e, parseTreeNode) {
+    check(e.type === "boolean", `Expected boolean`, parseTreeNode);
+  }
+
+  function checkNotDeclared(name, parseTreeNode) {
+    check(
+      !locals.has(name),
+      `Variable already declared: ${name}`,
+      parseTreeNode
+    );
+  }
+
+  function checkDeclared(name, parseTreeNode) {
+    check(locals.has(name), `Undeclared variable: ${name}`, parseTreeNode);
   }
 
   const translator = grammar.createSemantics().addOperation("translate", {
@@ -32,14 +52,19 @@ export default function translate(match) {
       emit("break;");
     },
     VarDec(_let, id, _eq, exp, _semi) {
-      check(
-        !locals.has(id.sourceString),
-        `Variable already declared: ${id.sourceString}`,
-        id
-      );
+      checkNotDeclared(id.sourceString, id);
       const initializer = exp.translate();
-      locals.set(id.sourceString, "number");
-      emit(`let ${id.sourceString} = ${initializer};`);
+      const variable = {
+        kind: "variable",
+        name: id.sourceString,
+        mutable: true,
+        type: initializer.type,
+        toString() {
+          return this.name;
+        },
+      };
+      locals.set(id.sourceString, variable);
+      emit(`let ${variable.name} = ${initializer};`);
     },
     PrintStmt(_print, exp, _semi) {
       emit(`console.log(${exp.translate()});`);
@@ -50,7 +75,9 @@ export default function translate(match) {
       emit(`${variable} = ${value};`);
     },
     WhileStmt(_while, exp, block) {
-      emit(`while (${exp.translate()}) {`);
+      const test = exp.translate();
+      checkBoolean(test, exp);
+      emit(`while (${test}) {`);
       block.translate();
       emit("}");
     },
@@ -62,36 +89,61 @@ export default function translate(match) {
     Exp_test(left, op, right) {
       const targetOp =
         { "==": "===", "!=": "!==" }?.[op.sourceString] ?? op.sourceString;
-      return `${left.translate()} ${targetOp} ${right.translate()}`;
+      return `(${left.translate()} ${targetOp} ${right.translate()})`;
     },
     Condition_add(left, _op, right) {
-      return `${left.translate()} + ${right.translate()}`;
+      const x = left.translate();
+      const y = right.translate();
+      checkNumber(x, left);
+      checkNumber(y, right);
+      return {
+        type: "number",
+        toString() {
+          return `(${x} + ${y})`;
+        },
+      };
     },
     Condition_sub(left, _op, right) {
-      return `${left.translate()} - ${right.translate()}`;
+      return `(${left.translate()} - ${right.translate()})`;
     },
     Term_mul(left, _op, right) {
-      return `${left.translate()} * ${right.translate()}`;
+      return `(${left.translate()} * ${right.translate()})`;
     },
     Term_div(left, _op, right) {
       return `${left.translate()} / ${right.translate()}`;
     },
     Term_mod(left, _op, right) {
-      return `${left.translate()} % ${right.translate()}`;
+      return `(${left.translate()} % ${right.translate()})`;
     },
     Primary_parens(_open, exp, _close) {
       return exp.translate();
+    },
+    Factor_neg(_op, operand) {
+      return `-(${operand.translate()})`;
+    },
+    Factor_exp(left, _op, right) {
+      return `(${left.translate()} ** ${right.translate()})`;
     },
     numeral(digits, _dot, _fractional, _e, _sign, _exponent) {
       return Number(this.sourceString);
     },
     id(_first, _rest) {
-      const name = this.sourceString;
-      check(locals.has(name), `Undeclared variable: ${name}`, this);
-      return name;
+      const entity = locals.get(this.sourceString);
+      checkDeclared(this.sourceString, this);
+      return entity;
+    },
+    true(_) {
+      return true;
+    },
+    false(_) {
+      return false;
     },
   });
 
   translator(match).translate();
   return target;
 }
+
+Number.prototype.type = "number";
+Boolean.prototype.type = "boolean";
+String.prototype.type = "string";
